3c646bb5b9b8e05eee4c2d46f03407d00916bd8b
compnerd <compnerd@users.noreply.github.com>
2022-03-31 17:25:58 +0000

    deploy: e93f823c33c5d96696c38dc0ae216a9b24347cee

--- search/all_7.js
+++ search/all_7.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['handler_0',['Handler',['../structds2_1_1_g_d_b_remote_1_1_protocol_interpreter_1_1_handler.html',1,'ds2::GDBRemote::ProtocolInterpreter']]],
-  ['hardwarebreakpointmanager_1',['HardwareBreakpointManager',['../classds2_1_1_hardware_breakpoint_manager.html',1,'ds2']]],
-  ['hostinfo_2',['HostInfo',['../structds2_1_1_g_d_b_remote_1_1_host_info.html',1,'ds2::GDBRemote::HostInfo'],['../structds2_1_1_host_info.html',1,'ds2::HostInfo']]]
+  ['handlechannel_0',['HandleChannel',['../classds2_1_1_host_1_1_handle_channel.html',1,'ds2::Host']]],
+  ['handler_1',['Handler',['../structds2_1_1_g_d_b_remote_1_1_protocol_interpreter_1_1_handler.html',1,'ds2::GDBRemote::ProtocolInterpreter']]],
+  ['hardwarebreakpointmanager_2',['HardwareBreakpointManager',['../classds2_1_1_hardware_breakpoint_manager.html',1,'ds2']]],
+  ['hostinfo_3',['HostInfo',['../structds2_1_1_g_d_b_remote_1_1_host_info.html',1,'ds2::GDBRemote::HostInfo'],['../structds2_1_1_host_info.html',1,'ds2::HostInfo']]]
 ];
